0adc5f499c35893946dac691a99ee13de6acc449
Byrd, Grayson B <grayson.byrd@jhuapl.edu>
2024-08-15 12:43:30 -0400

    Updated to structured layout

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,14 @@
+return {
+  -- {
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   ft = {"python"},
+  --   opts = function()
+  --     local null_ls = require('null-ls')
+  --     local sources = {
+  --       null_ls.builtins.diagnostics.mypy,
+  --       null_ls.builtins.diagnostics.ruff,
+  --     }
+  --     return sources
+  --   end
+  -- }
+}
